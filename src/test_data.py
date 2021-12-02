3bd9339d06283e6b05ccf7dd710c853608af153d
marcos-vas <marcosvaas@gmail.com>
2021-12-02 08:46:43 -0300

    tests: altera caminho para rodar com o workflow

--- src/test_data.py
+++ src/test_data.py
@@ -4,8 +4,8 @@ from data import load
 
 
 file_names = [
-    "output_test/membros-ativos-contracheque-02-2020.ods",
-    "output_test/membros-ativos-verbas-indenizatorias-02-2020.ods",
+    "src/output_test/membros-ativos-contracheque-02-2020.ods",
+    "src/output_test/membros-ativos-verbas-indenizatorias-02-2020.ods",
 ]
 
 
@@ -14,7 +14,7 @@ class TestData(unittest.TestCase):
     def test_validate_existence(self):
         STATUS_DATA_UNAVAILABLE = 4
         with self.assertRaises(SystemExit) as cm:
-            dados = load(file_names, '2021', '01' , './output_test')
+            dados = load(file_names, '2021', '01' , 'src/output_test')
             dados.validate()
         self.assertEqual(cm.exception.code, STATUS_DATA_UNAVAILABLE)
 
